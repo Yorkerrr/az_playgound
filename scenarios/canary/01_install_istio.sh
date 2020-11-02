f2a311274db10253bc08ac5ac8f32cf08b666440
Vitalii Burak <vburak@griddynamics.com>
2020-11-02 14:07:20 -0800

    orange

--- scenarios/canary/01_install_istio.sh
+++ scenarios/canary/01_install_istio.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 . utils.sh
 
-istioctl install --set profile=demo --context ${MAIN_CLUSTER_CTX}
+istioctl install --set profile=default --context ${MAIN_CLUSTER_CTX}
+kubectl apply -f ./addons/prometheus.yaml -n istio-system --context=${MAIN_CLUSTER_CTX}
 kubectl label namespace default istio-injection=enabled --context ${MAIN_CLUSTER_CTX}
