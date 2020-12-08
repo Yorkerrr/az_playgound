85e035eb4d673de839575038b784112f626ec8a3
Vitalii Burak <vburak@griddynamics.com>
2020-12-07 16:08:54 -0800

    update

--- scenarios/multicluster/shared_control_plane_scenario/verify.sh
+++ scenarios/multicluster/shared_control_plane_scenario/verify.sh
@@ -27,9 +27,9 @@ kubectl exec --context="${REMOTE_CLUSTER_CTX}" -n sample -c sleep-secondary \
 
 ISTIOD_PRIMARY_GW=$(kubectl get svc -n istio-system --context=${MAIN_CLUSTER_CTX} istio-secondary-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-echo "Verify Secondary cluster Sleep to Secondary cluster via primary ingress gw:"
+# echo "Verify Secondary cluster Sleep to Secondary cluster via primary ingress gw:"
 
-kubectl exec --context="${REMOTE_CLUSTER_CTX}" -n sample -c sleep-secondary \
-    "$(kubectl get pod --context="${REMOTE_CLUSTER_CTX}" -n sample -l \
-    app=sleep-secondary -o jsonpath='{.items[0].metadata.name}')" \
-    -- sh -c 'for i in `seq 1 10`; do curl -s helloworld-primary.sample.global:5000/hello; done'
+# kubectl exec --context="${REMOTE_CLUSTER_CTX}" -n sample -c sleep-secondary \
+#     "$(kubectl get pod --context="${REMOTE_CLUSTER_CTX}" -n sample -l \
+#     app=sleep-secondary -o jsonpath='{.items[0].metadata.name}')" \
+#     -- sh -c 'for i in `seq 1 10`; do curl -s helloworld-primary.sample.global:5000/hello; done'
